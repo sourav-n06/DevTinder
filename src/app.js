9cb16bbc0c3e0223f2e5b76348cfe2571b9e8356
Sourav Hazra <souxhaz06@gmail.com>
2025-02-23 13:05:34 +0530

    Created a Express Server

--- src/app.js
+++ src/app.js
@@ -2,6 +2,17 @@
 
  const app = express();
 
+ // This will only Handle GET API Calls of User
+ app.get("/user", (req, res) => {
+      res.send({firstname : "Sourav", lastname : "Hazra"});
+ })
+
+// This will only Handle POST API Calls of User
+ app.post("/user", (req, res) => {
+   res.send("Data Successfully Saved to the DataBase...");
+})
+
+ // This will match all the HTTP method API calls (GET and POST)
  app.use("/", (req, res) => {
     res.send("Hello from Server Side first time...");
  })
